2fbe9819d17690e2a2e692555c77646c7274994a
Alex Yu <alex@young-yu.com>
2017-09-12 17:57:21 -0400

    Add missing groupId param to updateGroup documentation

--- index.js
+++ index.js
@@ -330,6 +330,7 @@ class TrueVaultClient {
 
     /**
      * Update an existing group's name and policy. See https://docs.truevault.com/groups#update-a-group.
+     * @param {string} groupId group id to update.
      * @param {string} name group name.
      * @param {Object} policy group policy. See https://docs.truevault.com/groups.
      * @returns {Promise.<Object>}
