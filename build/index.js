60cb908e404e1ba9885c6784541d618a0d3eb43b
Manuel Wudka-Robles <mwudka@me.com>
2017-09-27 20:05:16 -0700

    Bump version to 0.8.0

--- build/index.js
+++ build/index.js
@@ -13341,7 +13341,7 @@ module.exports = function(module) {
 module.exports = {
 	"name": "truevault",
 	"description": "The official TrueVault JavaScript SDK",
-	"version": "0.7.0",
+	"version": "0.8.0",
 	"repository": {
 		"type": "git",
 		"url": "https://github.com/truevault/truevault-js-sdk"
